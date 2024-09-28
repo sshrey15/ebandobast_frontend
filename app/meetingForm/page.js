31fb72929439c20e4a868413bf90f81b9d21688f
vishnu variyar <vishnuvariyar04@gmail.com>
2024-09-28 23:46:26 +0530

    meetingform-patrollingform

---
+++ app/meetingForm/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MeetingForm from '../components/MeetingForm'
+const page = () => {
+  return (
+    <div>
+            <MeetingForm/>
+    </div>
+
+  )
+}
+
+export default page
